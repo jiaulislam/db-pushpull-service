19623d3a2d403c76e52f439ab8e004a3c7b65c63
jiaulislam <kmjiaulislam@gmail.com>
2022-09-18 10:47:20 +0600

    added comments in main.py

--- src/main.py
+++ src/main.py
@@ -15,5 +15,8 @@ if __name__ == "__main__":
 
     _end_datetime = datetime(today.year, today.month, today.day, 23, 59, 59)
 
+    # create the logs from sql server to oracle
     pp.core_create_logs(_start_datetime, _end_datetime)
+
+    # update the logs on both sql server and oracle
     pp.core_update_logs(_start_datetime, _end_datetime, 100)
